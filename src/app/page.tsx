cca243b677e2b8d292092dd766015c2139d35992
Luis Ortiz <l.ortizintec@gmail.com>
2023-09-21 22:01:36 -0400

    added basic conditional rendering of the login and register forms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import Login from "../components/Login";
+import Register from "@/components/Register";
 import { useEffect, useState } from 'react';
 
 export default function Landing() {
 
   const [data, setData] = useState([]);
+  const [loginOrRegister, setLoginOrRegister] = useState('');
 
   useEffect(() => {
     fetch('/api/getData', {
@@ -15,14 +17,32 @@ export default function Landing() {
       .catch((error) => console.error('Error fetching data:', error));
   }, []);
 
-  console.log(data);
-  
+  const handleAction = () => {
+    if (loginOrRegister === 'Register') {
+      return <Register />
+    } else if (loginOrRegister === 'Login') {
+      return <Login />
+    } else { return <h1>Login or Register</h1> }
+  }
+
 
   return (
     <div>
       <h1 className='text-red-600'>Landing Page</h1>
       <h1>All Users: {JSON.stringify(data)}</h1>
-      <Login />
+
+      <button
+        className="m-1 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md"
+        onClick={() => setLoginOrRegister('Login')}>
+        Login
+      </button>
+      <button 
+      className="m-1 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md"
+      onClick={() => setLoginOrRegister('Register')}>
+        Register
+        </button>
+      {handleAction()}
+
     </div>
   )
 }
